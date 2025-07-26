6913f56288da26ab29f4584b4ad4026b12f3c812
Kenneth Okereke <kennethokereke@Kenneths-MacBook-Air.local>
2025-07-26 11:01:39 -0400

    Replace with proper Spring Boot Maven project

---
+++ src/test/java/com/example/url_safety_checker/UrlSafetyCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.url_safety_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlSafetyCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
